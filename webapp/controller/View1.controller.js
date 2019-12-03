64a078dca7d20153190a3abdc1fbeb39e673d288
luis-barros <luigi.barros@gmail.com>
2019-12-03 10:29:54 +0100

    Final commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,6 +4,20 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("com.fcaCorsoSCP.controller.View1", {
-
+		onItemPress: function (oEvent) {
+			var oContext = oEvent.getSource().getBindingContext();
+			this.getView().byId("page2").setBindingContext(oContext);
+			this.getView().byId("app").to(this.getView().byId("page2"));
+		},
+		onSave: function(oEvent) {
+			this.getView().getModel().submitChanges();
+		},
+		onNew: function(oEvent) {
+			this.getView().byId("app").to(this.getView().byId("page2"));
+			this.getView().byId("username").setEditable(true);
+			var oModel = this.getView().getModel();
+			var oContext = oModel.createEntry("/PeopleSet");
+			this.getView().byId("page2").setBindingContext(oContext);
+		}
 	});
 });
